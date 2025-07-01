62776d6a963c0ceb9eb72f5d4c88db787fff27ef
Pannathon Saman <pannathon@idpassglobal.net>
2025-07-01 10:23:35 +0700

    add CheckCondition-NewOrder

--- pages/kyc-page.ts
+++ pages/kyc-page.ts
@@ -1,4 +1,4 @@
-import { Locator, Page } from "@playwright/test";
+import { expect, Locator, Page } from "@playwright/test";
 
 export class KYCPage{
     readonly page: Page;
@@ -6,13 +6,18 @@ export class KYCPage{
     readonly takePhotoButton: Locator;
     readonly nextButton: Locator;
     readonly custInfoNextButton: Locator;
+    readonly NewOrder: Locator;
+    readonly CheckCondition: Locator;
 
     constructor(page: Page) {
         this.page = page;
         this.chooseTakePhoto = page.getByRole('button', { name: ' ถ่ายรูปบัตร' });
         this.takePhotoButton = page.getByTestId('captureIdCard');
-        this.nextButton = page.getByTestId('buttonNext');
+        this.nextButton = page.getByTestId('buttonNext');     
         this.custInfoNextButton = page.getByTestId('buttonNext');
+        
+        this.CheckCondition = page.getByText('ต้องการทำรายการเดิมหรือไม่?');
+        this.NewOrder = page.getByTestId('buttonCreateNewOrder');
 
     }
 
@@ -20,6 +25,10 @@ export class KYCPage{
         await this.chooseTakePhoto.click();
         await this.takePhotoButton.click();
         await this.nextButton.click();
+        
+        if (await this.CheckCondition.isVisible()) {
+            await this.NewOrder.click();
+        }
     } 
 
     async CheckCustInfo() {
